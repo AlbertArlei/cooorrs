79728b4caca951d3007f14dedf687433acf7cb23
albert arlei <albertarlei364@gmail.com>
2023-05-15 22:09:57 -0300

    update

--- src/index.php
+++ src/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> - </title>
+</head>
+<body>
 <?php
 
 require "./router/router.php";
@@ -5,15 +14,34 @@ require "./router/router.php";
 $router = new Router();
 
 $router->addRoute('GET', '/', function() {
-     include('./pages/a.php');
+    include('./pages/home.php');
+});
+
+$router->addRoute('GET', '/services', function() {
+    include('./pages/services.php');
 });
 
 $router->addRoute('GET', '/about', function() {
-    echo "Sobre nós";
+    include('./pages/about');
 });
 
-$router->addRoute('GET', '/contact', function() {
-    echo "Entre em contato conosco";
+$router->addRoute('GET', '/about', function() {
+    include('./pages/about');
+});
+
+$router->addRoute('GET', '/login', function() {
+    include('./pages/about');
+});
+
+$router->addRoute('GET', '/about', function() {
+    include('./pages/about');
+});
+
+$router->addRoute('GET', '/about', function() {
+    include('./pages/about');
 });
 
 $router->run($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+?>
+</body>
+</html>
